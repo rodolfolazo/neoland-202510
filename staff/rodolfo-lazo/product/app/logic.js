6e282181db75bd622bbcae2534d7308c211661e8
Rodolfo Lazo <rodolfo.lazo@outlook.com>
2025-11-25 21:24:48 +0000

    Add Logic register user

--- staff/rodolfo-lazo/product/app/logic.js
+++ staff/rodolfo-lazo/product/app/logic.js
@@ -6,6 +6,25 @@ Logic.prototype.registerUser = function (
   username,
   password,
   paasswordRepeat
-) {};
+) {
+  //validations
+  if (!name || !email || !username || !password || !paasswordRepeat) {
+    throw new Error("All fields are required");
+  }
+  if (password !== paasswordRepeat) {
+    throw new Error("Passwords do not match");
+  }
+  //create user
+  const user = new User(
+    "user" + (data.usersCount + 1),
+    name,
+    email,
+    username,
+    password,
+    "user"
+  );
+  data.insertUser(user);
+  data.usersCount++;
+};
 
 const logic = new Logic();
